ef99b6f530d3d9548d7c81ce2ae0a7ef36bfca6e
ezratawil <83072159+ezratawil@users.noreply.github.com>
2022-01-10 11:30:12 +0200

    Update data_curation.py

--- data_curation.py
+++ data_curation.py
@@ -5,18 +5,21 @@ import tensorflow_datasets as tfds
 from datasets import load_dataset
 from sklearn.model_selection import train_test_split
 
-
+# IMDB Movie Reviews Dataset
 imdb, info = tfds.load('imdb_reviews', with_info=True, split='train+test')
 imdb= tfds.as_dataframe(imdb,info)
 imdb['text'] = imdb['text'].str.decode('utf-8') # decode from bytestring
 imdb['website'] = 'imdb' # keeping track of the website for later use
 imdb = imdb[['website','text','label']]
-
+imdb.to_csv('imdb_proc.csv')
+# Yelp Restaurant Reviews Dataset
 yelp, info = tfds.load('yelp_polarity_reviews', with_info=True, split='train+test')
 yelp= tfds.as_dataframe(yelp,info)
 yelp['text'] = yelp['text'].str.decode('utf-8')
 yelp['website'] = 'yelp'
 yelp = yelp[['website','text','label']]
+yelp.to_csv('yelp.csv')
+# Amazon Product Reviews Dataset
 # load 4000000 amazon reviews from huggingface 
 amazon_data = load_dataset("amazon_polarity") 
 amazon_data['train'].to_csv('amazon_train.csv')
@@ -30,7 +33,7 @@ amazon.drop(['title','Unnamed: 0'],axis=1,inplace=True)
 amazon.rename(columns={'content': 'text'},inplace=True)
 amazon['website'] = 'amazon'
 amazon = amazon[['website','text','label']]
-
+amazon.to_csv('amazon.csv')
 
 
 
